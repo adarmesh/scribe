56509adb0d45515b640c9d66aaa097969305b290
Aidar Darmesh <darmesh.aidar@gmail.com>
2026-01-17 23:05:13 +0500

    Converted to Safari extension

---
+++ Stepify - SOPs, Guides, and Documentation/Stepify - SOPs, Guides, and Documentation/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Stepify - SOPs, Guides, and Documentation
+//
+//  Created by Aidar Darmesh on 17/01/2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
